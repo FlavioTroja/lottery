4fce5a01350428bac9ec122ea838b7db2be534b7
Flavio Troia <flavio.troia@assistdigital.com>
2023-07-27 15:47:23 +0200

    fix: uterate

--- scripts/iterate.js
+++ scripts/iterate.js
@@ -2,17 +2,17 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 (async function() {
 
-    const year = 2022;
+    const year = 2020;
     
     try {
         console.log("Starting iterate process @ ");
 
-        for (let i = 101; i <= 200; i++) {
+        for (let i = 1; i <= 156; i++) {
 
             process.stdout.write(`iterating ${i}/${year} `);
             await fetch(`http://localhost:3000/api/cron?year=${year}&ext=${i}`);
             process.stdout.write("Done!\n");
-            await delay(5000);
+            //await delay(5000);
         }
     } finally {
         console.log("Closing iterate process");
